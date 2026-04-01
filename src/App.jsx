aa08736989402caa2ec705ecc212e34c4496ad7f
Breschi <breschicaua426@gmail.com>
2026-04-01 07:34:40 -0300

    requiem

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Topo from './components/Topo'
+import Capa from './components/Capa'
+
+function App() {
+  return (
+    <div>
+      <header>
+        <Topo/>
+      </header>
+      <main>
+        <Capa/>
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Capa/index.jsx
@@ -0,0 +1,17 @@
+import './Capa.css';
+import banner from '../../assets/banner.jpg'
+import bannermobile from '../../assets/banner-mobile.jpg'
+
+export default function Capa(){
+    return (
+        <section className='secao_capa' style={{
+            backgroundImage: `url(${banner})`,
+        }}>
+            <div className='texto'>
+                <h2>Preview</h2>
+                <h1>Requiem</h1>
+                <button className='btn'>Comprar</button>
+            </div>
+        </section>
+    )
+}
